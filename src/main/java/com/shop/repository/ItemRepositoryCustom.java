58cf2ed9e0a4dcbc7f5a9449cd1db10cb7108933
fulfe <fulfeel2@gmail.com>
2023-02-04 23:34:13 +0900

    shop project version1

---
+++ src/main/java/com/shop/repository/ItemRepositoryCustom.java
@@ -0,0 +1,12 @@
+package com.shop.repository;
+
+//import com.shop.dto.ItemSearchDto;
+//import com.shop.dto.MainItemDto;
+
+public interface ItemRepositoryCustom {
+
+//    Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+
+//    Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+
+}
