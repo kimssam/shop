eca0bd0200e00cc6d5af752758cd933766410cb9
fulfe <fulfeel2@gmail.com>
2023-02-05 02:25:45 +0900

    shop project version6 ~p.234 6.상품등록 및 조회하기(도중)

--- src/main/java/com/shop/dto/ItemImgDto.java
+++ src/main/java/com/shop/dto/ItemImgDto.java
@@ -1,6 +1,7 @@
 package com.shop.dto;
 
 //import com.shop.entity.ItemImg;
+import com.shop.entity.ItemImg;
 import lombok.Getter;
 import lombok.Setter;
 import org.modelmapper.ModelMapper;
@@ -13,8 +14,8 @@ public class ItemImgDto {
     private String imgUrl;
     private String repImgYn;
     private static ModelMapper modelMapper = new ModelMapper();
-//    public static ItemImgDto of(ItemImg itemImg){
-//        return modelMapper.map(itemImg, ItemImgDto.class);
-//    }
+    public static ItemImgDto of(ItemImg itemImg){
+        return modelMapper.map(itemImg, ItemImgDto.class);
+    }
 
 }
